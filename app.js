64479ad0d568fe47b66d7d0a4ad08a42bb890e73
Miguel T Rivera <10368105+mtrivera@users.noreply.github.com>
2025-10-15 23:59:19 -0700

    feat: Add getComputerChoice() logic

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 "use strict";
+
+function getComputerChoice() {
+  const randomNum = Math.floor(Math.random() * 3) + 1;
+
+  let computerMove;
+
+  switch(randomNum) {
+    case 1:
+      computerMove = 'paper';
+      break;
+    case 2:
+      computerMove = 'rock';
+      break;
+    case 3:
+      computerMove = 'scissors';
+      break;
+  }
+  return computerMove;
+}
